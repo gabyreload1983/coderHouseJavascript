1c41a5f800321a6d965621a01f60b108285e1f57
Gabriel Godoy <gabyreload@gmail.com>
2022-09-15 18:54:56 -0300

    Desafío: Incorporar Arrays

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,57 +1,66 @@
-// ############################
-function calcularTotal(precio, cuotas) {
-  switch (cuotas) {
-    case 3:
-      return (precio * 1.1).toFixed(2);
-    //   break;
-    case 6:
-      return (precio * 1.15).toFixed(2);
-    //   break;
-    case 12:
-      return (precio * 1.2).toFixed(2);
-    //   break;
+class Producto {
+  constructor(product) {
+    this.id = product.id;
+    this.description = product.description;
+    this.brand = product.brand;
+    this.categorie = product.categorie;
+    this.price = product.price;
+    this.stock = product.stock;
+    this.offer = false;
+  }
+  checkId(id) {
+    return this.id === id;
+  }
+  sumarIva() {
+    return this.price * 1.21;
   }
 }
 
-function calcularCuota(total, cuotas) {
-  return (total / cuotas).toFixed(2);
-}
-
-function solicitarNumeroPositivo(mensaje) {
-  let numero = Number(prompt(mensaje));
-  while (numero <= 0 || isNaN(numero)) {
-    numero = Number(prompt(mensaje));
+const searchProduct = (id) => {
+  for (const product of products) {
+    if (product.checkId(id)) {
+      return product;
+    }
   }
-  return numero;
+  return false;
+};
+
+const insertNumber = (text) => {
+  let num = Number(prompt(text));
+  while (num <= 0 || isNaN(num)) {
+    num = Number(prompt(text));
+  }
+  return num;
+};
+
+const products = [];
+
+let answer = prompt(
+  "Ingresar productos?? (yes / any key to exit): "
+).toLowerCase();
+
+while (answer === "yes" || answer === "y") {
+  const product = new Producto({
+    id: insertNumber("Ingrese id"),
+    description: prompt("Ingrese description"),
+    brand: prompt("Ingrese brand "),
+    categorie: prompt("Ingrese categorie"),
+    price: insertNumber("Ingrese price "),
+    stock: insertNumber("Ingrese stock"),
+  });
+  products.push(product);
+  answer = prompt(
+    "Ingresar nuevo producto?? (yes / any key to exit): "
+  ).toLowerCase();
 }
 
-let precio = solicitarNumeroPositivo("Ingrese precio: ");
+alert(`Se ingresaron ${products.length} productos`);
 
-let cuotas = solicitarNumeroPositivo(
-  "Seleccione cantidad de cuotas: 3 - 6 - 12 "
-);
-while ((cuotas !== 3 && cuotas !== 6 && cuotas !== 12) || isNaN(cuotas)) {
-  cuotas = solicitarNumeroPositivo(
-    "Seleccione cantidad de cuotas: 3 - 6 - 12 "
-  );
+if (products.length) {
+  let id = Number(prompt(`Buscar producto por ID: `));
+  const product = searchProduct(id);
+  if (product) {
+    alert(`El id ${id} pertenece al producto ${product.description}.
+    EL precio con IVA es $${product.sumarIva()}`);
+  }
 }
-
-let total = calcularTotal(precio, cuotas);
-alert(`El total a pagar con recargo es: $${total}`);
-alert(`${cuotas} cuotas de $${calcularCuota(total, cuotas)}`);
-
-// ################################################
-const agregarIva = (precio) => (precio * 1.21).toFixed(2);
-const calcularDescuento = (precio, descuento) =>
-  (precio - (precio * descuento) / 100).toFixed(2);
-
-let precioSinIva = solicitarNumeroPositivo("Ingrese precio sin iva:");
-alert(`El precio con iva es:${agregarIva(precioSinIva)}`);
-
-let descuento = solicitarNumeroPositivo("Ingrese porcentaje de descuento: ");
-alert(
-  `El precio con descuento es:${calcularDescuento(
-    agregarIva(precioSinIva),
-    descuento
-  )}`
-);
