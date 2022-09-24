835000a8f462a9f54073f64e763166dedf393253
Gabriel Godoy <gabyreload@gmail.com>
2022-09-24 16:49:34 -0300

    Se aplica spinner  de espera y se muestra una respuesta al enviar el formulario con exito

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -6,25 +6,32 @@ class Contact {
     this.email = user.email;
     this.message = user.message;
   }
-  sendMessage() {
-    return true;
-  }
 }
 
-const contact = () => {
-  const contact = new Contact({
-    firstName: prompt("Ingrese su nombre"),
-    lastName: prompt("Ingrese su apellido"),
-    email: prompt("Ingrese su email"),
-    message: prompt("Ingrese su consulta"),
-  });
-  if (contact.sendMessage()) {
+const validateForm = (e) => {
+  e.preventDefault();
+  const spinnerBorder = document.querySelector(".spinner-border");
+  spinnerBorder.classList.remove("visually-hidden");
+  setTimeout(() => {
+    const form = e.target.children;
+    const contact = new Contact({
+      firstName: form[0].children[1].value,
+      lastName: form[1].children[1].value,
+      email: form[2].children[1].value,
+      message: form[3].children[1].value,
+    });
+    e.target.reset();
+
     const messegeResponse = document.querySelector("#messegeResponse");
     messegeResponse.classList.remove("d-none");
     messegeResponse.innerHTML = `
-     ${contact.firstName} ${contact.lastName}, recibimos tu mensaje con exito. 
-     Nos comunicaremos con usted a la brevedad`;
-  }
+      <strong>${contact.firstName} ${contact.lastName}, recibimos tu mensaje con exito. </br>
+      Nos comunicaremos con usted a la brevedad.
+      </strong>
+      `;
+    spinnerBorder.classList.add("visually-hidden");
+  }, 1000);
 };
 
-contact();
+const formContact = document.querySelector("#formContact");
+formContact.addEventListener("submit", validateForm);
--- assets/js/technicalAssistance.js
+++ assets/js/technicalAssistance.js
@@ -6,26 +6,33 @@ class Ticket {
     this.email = user.email;
     this.message = user.message;
   }
-  createTicket() {
-    return true;
-  }
 }
 
-const technicalAssistance = () => {
-  const ticket = new Ticket({
-    firstName: prompt("Ingrese su nombre"),
-    lastName: prompt("Ingrese su apellido"),
-    celphone: prompt("Ingrese su celular"),
-    email: prompt("Ingrese su email"),
-    message: prompt("Detalle la falla"),
-  });
-  if (ticket.createTicket()) {
+const validateForm = (e) => {
+  e.preventDefault();
+  const spinnerBorder = document.querySelector(".spinner-border");
+  spinnerBorder.classList.remove("visually-hidden");
+  setTimeout(() => {
+    const form = e.target.children;
+    const ticket = new Ticket({
+      firstName: form[0].children[1].value,
+      lastName: form[1].children[1].value,
+      celphone: form[2].children[1].value,
+      email: form[3].children[1].value,
+      message: form[4].children[1].value,
+    });
+    e.target.reset();
+
     const messegeResponse = document.querySelector("#messegeResponse");
     messegeResponse.classList.remove("d-none");
     messegeResponse.innerHTML = `
-     ${ticket.firstName} ${ticket.lastName}, tu ticket se genero con exito. 
-     Nos comunicaremos con usted a la brevedad`;
-  }
+      <strong>${ticket.firstName} ${ticket.lastName}, tu ticket fue creado con exito. </br>
+      Nos comunicaremos con usted a la brevedad.
+      </strong>
+      `;
+    spinnerBorder.classList.add("visually-hidden");
+  }, 1000);
 };
 
-technicalAssistance();
+const formTicket = document.querySelector("#formTicket");
+formTicket.addEventListener("submit", validateForm);
