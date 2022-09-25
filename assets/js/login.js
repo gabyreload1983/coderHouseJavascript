9c02b3242edb85ff5d591d75d7be14cec62a591f
Gabriel Godoy <gabyreload@gmail.com>
2022-09-25 19:09:09 -0300

    Login

--- assets/js/login.js
+++ assets/js/login.js
@@ -13,12 +13,26 @@ const formLogin = document.querySelector("#formLogin");
 formLogin.addEventListener("submit", (e) => {
   e.preventDefault();
   const credentials = { email: e.target[0].value, password: e.target[1].value };
-  const user = checkUser(credentials);
-  if (user) {
-    const userName = document.querySelector("#userName");
-    userName.innerText = user.firstName;
-    e.target.reset();
-  } else {
-    console.log("datos incorrectos");
-  }
+
+  const spinnerBorder = document.querySelector(".spinner-border");
+  spinnerBorder.classList.remove("visually-hidden");
+
+  const messegeResponse = document.querySelector("#messegeResponse");
+  messegeResponse.classList.add("d-none");
+
+  setTimeout(() => {
+    const user = checkUser(credentials);
+    if (user) {
+      const userName = document.querySelector("#userName");
+      userName.innerText = user.firstName;
+      e.target.reset();
+      spinnerBorder.classList.add("visually-hidden");
+    } else {
+      spinnerBorder.classList.add("visually-hidden");
+      messegeResponse.classList.remove("d-none");
+      messegeResponse.innerHTML = `
+      <p class="text-danger text-center m-0 py-2">Datos incorrectos</p>
+      `;
+    }
+  }, 1000);
 });
