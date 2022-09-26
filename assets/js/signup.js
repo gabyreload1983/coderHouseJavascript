e81e160aba466a0d7150f7011b0e8f75385d1bc2
Gabriel Godoy <gabyreload@gmail.com>
2022-09-25 21:27:24 -0300

    se agregan addEventListener para el registro
    tambien se agrega id userName en Navbar

--- assets/js/signup.js
+++ assets/js/signup.js
@@ -1,3 +1,5 @@
+import { dataBaseUsers } from "./dataBase.js";
+
 class User {
   constructor(user) {
     this.firstName = user.firstName;
@@ -6,27 +8,63 @@ class User {
     this.email = user.email;
     this.password = user.password;
   }
-  registerOk() {
-    alert(`${this.firstName}, gracias por registrarse!`);
-  }
 }
 
-const signup = () => {
-  const data = {
-    firstName: prompt("Ingrese su nombre"),
-    lastName: prompt("Ingrese su apellido"),
-    celphone: prompt("Ingrese su celular"),
-    email: prompt("Ingrese su email"),
-    password: prompt("Ingrese su password"),
-    confirmPassword: prompt("Confirme su password"),
-  };
-  while (data.password !== data.confirmPassword) {
-    alert("Las contraseñas no coinciden");
-    data.password = prompt("Ingrese su password");
-    data.confirmPassword = prompt("Confirme su password");
-  }
-  const user = new User(data);
-  user.registerOk();
+const checkUserExists = (email) => {
+  return dataBaseUsers.find((user) => user.email === email);
 };
 
-signup();
+const register = (user) => {
+  return true;
+};
+
+const formSignup = document.querySelector("#formSignup");
+formSignup.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const messegeResponse = document.querySelector("#messegeResponse");
+  messegeResponse.classList.add("d-none");
+
+  const spinnerBorder = document.querySelector(".spinner-border");
+  spinnerBorder.classList.remove("visually-hidden");
+
+  setTimeout(() => {
+    spinnerBorder.classList.add("visually-hidden");
+
+    const password = e.target[4].value;
+    const confirmPassword = e.target[5].value;
+
+    if (password !== confirmPassword) {
+      messegeResponse.classList.remove("d-none");
+      messegeResponse.innerHTML = `
+      <p class="text-danger text-center m-0 py-2">Contraseñas no coinciden</p>
+      `;
+      return null;
+    }
+    const email = e.target[3].value;
+    if (checkUserExists(email)) {
+      messegeResponse.classList.remove("d-none");
+      messegeResponse.innerHTML = `
+      <p class="text-danger text-center m-0 py-2">El email ya se encuentra registrado</p>
+      `;
+      return null;
+    }
+    const user = new User({
+      firstName: e.target[0].value,
+      lastName: e.target[1].value,
+      celphone: e.target[2].value,
+      email: e.target[3].value,
+      password: e.target[4].value,
+    });
+    const response = register(user);
+    if (response) {
+      messegeResponse.classList.remove("d-none");
+      messegeResponse.innerHTML = `
+      <p class="text-info text-center m-0 py-2">${user.firstName} ${user.lastName} te registraste con exito!</p>
+      `;
+      const userName = document.querySelector("#userName");
+      userName.innerText = user.firstName;
+      e.target.reset();
+    }
+  }, 1000);
+});
