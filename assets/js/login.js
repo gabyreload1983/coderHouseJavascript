ce908c5f14dc1481bbc1bca552a8941051213d3f
Gabriel Godoy <gabyreload@gmail.com>
2022-09-30 00:23:45 -0300

    Se crea navLogin.js
    Se agrega a todas las vistas navLogin.js y cart.js
    Se modifica header

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,30 +1,37 @@
 import { dataBaseUsers } from "./dataBase.js";
 
+const formLogin = document.querySelector("#formLogin");
+const spinnerBorder = document.querySelector(".spinner-border");
+const messegeResponse = document.querySelector("#messegeResponse");
+
 const checkUser = (credentials) => {
-  const user = dataBaseUsers.find(
+  let user = dataBaseUsers.find(
     (user) =>
       user.email === credentials.email && user.password === credentials.password
   );
+  if (user) {
+    user = {
+      firstName: user.firstName,
+      lastName: user.lastName,
+      email: user.email,
+    };
+  }
 
   return user;
 };
 
-const formLogin = document.querySelector("#formLogin");
 formLogin.addEventListener("submit", (e) => {
   e.preventDefault();
   const credentials = { email: e.target[0].value, password: e.target[1].value };
 
-  const spinnerBorder = document.querySelector(".spinner-border");
   spinnerBorder.classList.remove("visually-hidden");
-
-  const messegeResponse = document.querySelector("#messegeResponse");
   messegeResponse.classList.add("d-none");
 
   setTimeout(() => {
     const user = checkUser(credentials);
     if (user) {
-      const userName = document.querySelector("#userName");
-      userName.innerText = user.firstName;
+      sessionStorage.setItem("user", JSON.stringify(user));
+      renderNavLogin(user);
       e.target.reset();
       spinnerBorder.classList.add("visually-hidden");
     } else {
