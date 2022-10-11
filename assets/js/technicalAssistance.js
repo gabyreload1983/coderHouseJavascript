072b3e4fb886566056fa2a6c5becfd906802a035
Gabriel Godoy <gabyreload@gmail.com>
2022-10-10 23:21:38 -0300

    Se agrega fetch y API jsonplaceholder

--- assets/js/technicalAssistance.js
+++ assets/js/technicalAssistance.js
@@ -13,46 +13,57 @@ class Ticket {
   }
 }
 
-const validateForm = (e) => {
+const validateForm = async (e) => {
   e.preventDefault();
   spinnerBorderTechnicalAssistance.classList.remove("visually-hidden");
 
-  setTimeout(() => {
-    const form = e.target.children;
-    const ticket = new Ticket({
-      firstName: form[0].children[1].value,
-      lastName: form[1].children[1].value,
-      celphone: form[2].children[1].value,
-      email: form[3].children[1].value,
-      message: form[4].children[1].value,
-    });
+  const form = e.target.children;
+  const ticket = new Ticket({
+    firstName: form[0].children[1].value,
+    lastName: form[1].children[1].value,
+    celphone: form[2].children[1].value,
+    email: form[3].children[1].value,
+    message: form[4].children[1].value,
+  });
 
-    createTicket(ticket)
-      ? Swal.fire({
-          title: "Ticket creado",
-          text: `${ticket.firstName} ${ticket.lastName}, tu ticket fue creado con exito.
+  (await createTicket(ticket))
+    ? Swal.fire({
+        title: "Ticket creado",
+        text: `${ticket.firstName} ${ticket.lastName}, tu ticket fue creado con exito.
         Nos comunicaremos con usted a la brevedad.
         `,
-          icon: "success",
-          iconColor: "#ec811c",
-          confirmButtonColor: "#ec811c",
-        })
-      : Swal.fire({
-          title: "Error",
-          text: `${ticket.firstName} ${ticket.lastName}, no se pudo crear tu ticket.
+        icon: "success",
+        iconColor: "#ec811c",
+        confirmButtonColor: "#ec811c",
+      })
+    : Swal.fire({
+        title: "Error",
+        text: `${ticket.firstName} ${ticket.lastName}, no se pudo crear tu ticket.
         Intenta mas tarde.
       `,
-          icon: "error",
-          confirmButtonColor: "#e33",
-        });
+        icon: "error",
+        confirmButtonColor: "#e33",
+      });
 
-    e.target.reset();
-    spinnerBorderTechnicalAssistance.classList.add("visually-hidden");
-  }, 1000);
+  e.target.reset();
+  spinnerBorderTechnicalAssistance.classList.add("visually-hidden");
 };
 
 const createTicket = (ticket) => {
-  return true;
+  return new Promise((resolve, reject) => {
+    fetch("https://jsonplaceholder.typicode.com/posts", {
+      method: "POST",
+      body: JSON.stringify(ticket),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+    })
+      .then((response) => response.json())
+      .then((json) => {
+        resolve(json);
+      })
+      .catch((error) => console.log(error));
+  });
 };
 
 formTicket.addEventListener("submit", validateForm);
